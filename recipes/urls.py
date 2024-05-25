f59bde11ff63a7997136bf3192acc1c16d2f2a22
hvenanc <hvs.git@gmail.com>
2024-05-25 20:53:35 -0300

    Dados dinâmicos da própria base de dados da aplicação

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from recipes.views import *
 
+
+app_name = 'recipes'
+
 urlpatterns = [
     path('', home_view),
-    path('receitas/<int:id>/', recipes_view),
+    path('receitas/<int:id>/', recipes_view, name='recipes'),
+    path('receitas/categoria/<int:category_id>/', category_view, name="category")
 ]
 
 
--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
 
 
 def home_view(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(9)],
+        'recipes': recipes,
+    })
+
+
+def category_view(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
     })
 
 
