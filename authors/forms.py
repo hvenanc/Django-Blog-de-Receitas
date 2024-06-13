dbe94cd3933baff150d6eddbe829556ac90ec4b7
hvenanc <hvs.git@gmail.com>
2024-06-13 16:32:35 -0300

    Pequena correção na div da message e no arquivo de forms

--- authors/forms.py
+++ authors/forms.py
@@ -2,8 +2,17 @@ from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
-class RegisterForm(forms.Form):
-
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password',
+        ]
+        
     first_name = forms.CharField(
         required= True,
         label= 'Nome',
